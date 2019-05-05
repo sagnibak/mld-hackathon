bb5755eb2d87fab0b7115c9f3dc9dc41e8a99953
sagnibak <sagnikdps@gmail.com>
2019-05-05 10:49:32 -0700

    load data modified

--- load_data.py
+++ load_data.py
@@ -5,10 +5,16 @@ import numpy as np
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 catted_cache = None
 
-def get_train_data():
+def get_train_data(mode="mlp"):
     if catted_cache is None:
-        catted_cache = np.concatenate((x_train, x_test), axis=0)
-    return catted_cache
+        catted_cache = np.concatenate((x_train, x_test), axis=0
+                        ).astype(np.float32)
+        catted_cache = catted_cache / 127.5 - 1.
+
+    if mode == "cnn":
+        return catted_cache
+    if mode == "mlp":
+        return catted_cache.reshape(70_000, 784)
 
 
 def get_test_data():
--- ocnn_model.py
+++ ocnn_model.py
@@ -1,1 +1,7 @@
-import 
+from keras import backend as K
+from keras.models import Model
+from keras.layers import Input, Dense
+
+from load_data import get_train_data
+
+x_train = get_train_data(mode='mlp')
